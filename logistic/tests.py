7569487195bc07b0fa77a79a0a2c66b3ea14e448
andrei <freemail_2019@mail.ru>
2024-02-13 21:22:16 +0800

    Fatal

---
+++ logistic/tests.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Mytest(TestCase):
+    def test_ok(self):
+        assert True
--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
